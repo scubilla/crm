8928b988089e157705ea0e91439e89447875c01c
scubilla <simoncubilla76@gmail.com>
2024-01-11 12:19:26 -0300

    hasta 13 filtros
    hasta 13 filtros

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from .models import *
 from .forms import OrderForm
-
+from .filters import OrderFilter
 
 # Create your views here.
 
@@ -34,7 +34,10 @@ def customer(request, pk_test):
     orders = customer.order_set.all()
     order_count = orders.count()
 
-    context = {'customer': customer, 'orders': orders, 'order_count' : order_count}
+    myFilter = OrderFilter(request.GET, queryset=orders)
+    orders = myFilter.qs
+
+    context = {'customer': customer, 'orders': orders, 'order_count': order_count, 'myFilter': myFilter}
     return render(request, 'accounts/customer.html', context)
 
 ''' crea orden en una sola linea
